78f93a928897532e3fdd5b2149e74f9245ad28bc
Alex McIntosh <nz.a.mcintosh@gmail.com>
2018-10-02 16:10:44 +0100

    First commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,64 @@
+def return_10()
+  return 10
+end
+
+def add(number_1, number_2)
+  return number_1 + number_2
+end
+
+def subtract(number_1, number_2)
+  return number_1 - number_2
+end
+
+def multiply(number_1, number_2)
+  return number_1 * number_2
+end
+
+def divide(number_1, number_2)
+  return number_1 / number_2
+end
+
+def length_of_string(string)
+  string = "A string of length ca"
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(number_1, number_2)
+  return number_1.to_i + number_2.to_i
+end
+
+def number_to_full_name__month_1(month)
+   month_name = case number
+   when 1
+     "January"
+   when 2
+     "Febuary"
+   when 3
+     "March"
+   when 4
+     "April"
+   when 5
+     "May"
+   when 6
+     "june"
+   when 7
+     "July"
+   when 8
+     "August"
+   when 9
+     "September"
+   when 10
+     "october"
+   when 11
+     "november"
+   when 12
+     "December"
+  end
+end
+
+
+# def
